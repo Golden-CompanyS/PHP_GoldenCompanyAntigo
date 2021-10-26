4642fa65b4a5672b70f311d8965bde21ab98f2b0
Gustavo Pereira <gustavo.oito@hotmail.com>
2021-10-26 15:00:33 -0300

    Revert "Revert "Implementação do cadastro de músicas a partir do próprio site""
    This reverts commit 9a40a7d8eb6a4e8e38448ae6b9a9cac29844e612.

---
+++ history.php
@@ -0,0 +1,93 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Golden Company</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+		<link rel="sourtout icon" href="">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+	</head>
+	<body>
+		<?php include "header.html" ?>
+			<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel1">
+				<div class="carousel-inner">
+					<div class="carousel-item active">
+						<img style="width: 100%; height: 430.33px; object-fit: cover;" src="img/slideshow/1.jpg" alt="First slide">
+					</div>
+					<div class="carousel-item">
+						<img class="d-block w-100" src="img/slideshow/2.jpg" alt="Second slide">
+					</div>
+				</div>
+			</div>
+			
+			<div style="padding:25px;">
+				<div style="display:flex;align-items: center;">
+					<div>
+						<img src="" width="300px" height="300">
+					</div>
+					<div style="padding:20px;">
+						<h1>Nossa história</h1>
+						<p>Um grupo de 5 amigos que se encontraram enquanto estudavam sobre programação, descobriram 
+						compartilhar de um mesmo sonho: construir uma empresa de desenvolvimento de software e transformar
+						o futuro das empresas interessadas em se inserir em um mundo virtual, desenvolvendo softwares 
+						eficazes, seguros e qualificados, com facilidade de uso e com foco na melhor experiência para o 
+						usuário. Unindo as habilidades e a criatividade de cada um, começaram a desenvolver o esboço de 
+						sua empresa.
+						</p>
+						<p>A princípio, o projeto começou em encontros diários, onde realizavam leituras sobre 
+						administração, gestão de negócios e assuntos ligados a programação. Concentraram-se em moldar 
+						sua empresa, decidindo nome, que ficara como Golden Company, slogan e outros.
+						</p>
+						<p>Antes de iniciarem a sua divulgação, pensaram então em ter um exemplo do que iriam oferecer em
+						sua empresa, construindo um software do zero: pensaram em um sistema de gerenciamento de uma 
+						Livraria, que desenvolveram em 6 meses. Quando finalizaram o sistema, já com toda parte de design
+						e redes sociais em mãos, começaram a divulgar seu trabalho. Atualmente, a empresa já abrange boa 
+						parte do Brasil, mantendo seu foco e objetivos, transformando o futuro do mundo com o software.
+						</p>
+					</div>
+				</div>
+				
+				<div style="display:flex;align-items: center;">
+					<div style="padding:20px;">
+						<p>Um grupo de 5 amigos que se encontraram enquanto estudavam sobre programação, descobriram 
+						compartilhar de um mesmo sonho: construir uma empresa de desenvolvimento de software e transformar
+						o futuro das empresas interessadas em se inserir em um mundo virtual, desenvolvendo softwares 
+						eficazes, seguros e qualificados, com facilidade de uso e com foco na melhor experiência para o 
+						usuário. Unindo as habilidades e a criatividade de cada um, começaram a desenvolver o esboço de 
+						sua empresa.
+						</p>
+						<p>A princípio, o projeto começou em encontros diários, onde realizavam leituras sobre 
+						administração, gestão de negócios e assuntos ligados a programação. Concentraram-se em moldar 
+						sua empresa, decidindo nome, que ficara como Golden Company, slogan e outros.
+						</p>
+						<p>Antes de iniciarem a sua divulgação, pensaram então em ter um exemplo do que iriam oferecer em
+						sua empresa, construindo um software do zero: pensaram em um sistema de gerenciamento de uma 
+						Livraria, que desenvolveram em 6 meses. Quando finalizaram o sistema, já com toda parte de design
+						e redes sociais em mãos, começaram a divulgar seu trabalho. Atualmente, a empresa já abrange boa 
+						parte do Brasil, mantendo seu foco e objetivos, transformando o futuro do mundo com o software.
+						</p>
+					</div>
+					<div>
+						<img src="" width="300px" height="300">
+					</div>
+				</div>
+			</div>
+			
+			<section id="Funcs" style="display:flex;align-items: center;flex-direction: column;">
+				<div class="row">
+				  <div class="col">col</div>
+				  <div class="col">col</div>
+				  <div class="col">col</div>
+				  <div class="col">col</div>
+				</div>
+				<div class="row">
+				  <div class="col">col</div>
+				  <div class="col">col</div>
+				  <div class="col">col</div>
+				  <div class="col">col</div>
+				</div>
+			</section>
+		<?php include "footer.html" ?>
+	</body>
+</html>
